725527098192fa50e0b26be10b3337f5a4a1087b
RajanJS <rajanmaharjanmails@gmail.com>
2016-02-24 00:06:41 +0545

    implemented promises

--- location.js
+++ location.js
@@ -2,15 +2,17 @@ var request = require('request');
 var url ='http://ipinfo.io/';
 
 module.exports = function (callback) {
-	request({
+	return new Promise(function(resolve, reject){
+		request({
 		url: url,
 		json: true
-	},function(error, response, body){
-		if(error){
-			callback('Unable to fetch the location!');
-		}else{
-			callback(body);
-		}
+	},
+	function(error, response, body){
+			if(error){
+				reject('Unable to fetch the location!');
+			}else{
+				resolve(body);
+			}
+		});
 	});
-
 }
